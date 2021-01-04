f6ab5bc6ea2eb483a617058154f21ce8b1e37dcf
mElhadki <elhadkimariem3@gmail.com>
2021-01-04 17:23:45 +0100

    Part Product Front-end

---
+++ V2.0/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+var bodyParser=require('body-parser');
+const port = 8000;
+const mysql = require('mysql');
+var opn= require('opn');
+
+//connect with database
+
+const connection = mysql.createConnection({
+
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'crudnode'
+
+});
+
+//test Connection 
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+// Setting up the public directory
+// Configuration
+
+app.use(bodyParser.urlencoded({    //obligatoire 
+    extended: true
+ }));                 
+ 
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.listen(port, () => {console.log(`listening on port ${port}!`);
+//opn("http://localhost:8000/vue/Dashboard.html")
+});

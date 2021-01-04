91d2e452eecf2c3be2f3c1803e496fec06627e55
E-Abdelouahab <abdelouahabelhassnaoui36@gmail.com>
2021-01-04 17:24:15 +0100

    new update

--- v1.0/app.js
+++ v1.0/app.js
@@ -4,7 +4,6 @@ var bodyParser=require('body-parser');
 const port = 3000;
 var opn= require('opn');
 var chalk = require ('chalk');
-
 var mysql = require ('mysql');
 
 
@@ -18,7 +17,6 @@ app.use(bodyParser.urlencoded({    //obligatoire
 app.use(bodyParser.json());
 
 app.use(express.static('public'))
-
 // middleware
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -33,7 +31,7 @@ const con = mysql.createConnection({
 
 
 app.listen(port, () => {console.log(`listening on port ${port}!`);
-//opn("http://localhost:3000/vue/Dashboard.html")
+opn("http://localhost:3000/vue/Dashboard.html")
 });
 
 // if not connect 
@@ -42,3 +40,27 @@ con.connect(function(error){
     if(!!error) console.log(error);
     else console.log('Database is Connected');
 }); 
+
+//add
+app.get('/', (req, res, next) => {
+    const sql = "SELECT * FROM category ";
+    const query = con.query(sql, (err, result) => {
+        if(err)throw err;
+            res.render('/Dashboard', {
+                    pageTitle : 'Category',
+                    items : result,
+        
+            }); 
+        });       
+    });
+    app.get('/addc',(req,res, next)=>{
+        let sql = "SELECT * FROM  category,product";
+        let query = con.query(sql, (err, result) => {
+            if(err) throw err;
+            res.render('/addc', {
+                pageTitle : 'Add new category',
+                items : result
+            });
+        });
+
+    });

589f22a2e5a0d10fd5ff267b646b9b70475fcd84
E-Abdelouahab <abdelouahabelhassnaoui36@gmail.com>
2021-01-04 15:10:58 +0100

    add connection db

--- Application/app.js
+++ Application/app.js
@@ -3,6 +3,10 @@ const app = express();
 var bodyParser=require('body-parser');
 const port = 3000;
 var opn= require('opn');
+var chalk = require ('chalk');
+
+var mysql = require ('mysql');
+
 
 // Setting up the public directory
 // Configuration
@@ -15,7 +19,26 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'))
 
+// middleware
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//connection db
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin1",
+    database: "crudnode"
+});
+
 
 app.listen(port, () => {console.log(`listening on port ${port}!`);
 //opn("http://localhost:3000/vue/Dashboard.html")
 });
+
+// if not connect 
+
+con.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database is Connected');
+}); 
